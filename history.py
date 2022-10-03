5dac35e488bee830c34b8848da3fcf6ee6107cec
Sidhharth Sahu <110850678+SidhharthSahu@users.noreply.github.com>
2022-10-03 23:35:49 +0530

    Files Uploded

---
+++ history.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+runfile('C:/Users/sidhh/.spyder-py3/app.py', wdir='C:/Users/sidhh/.spyder-py3')
+debugfile('C:/Users/sidhh/.spyder-py3/app.py', wdir='C:/Users/sidhh/.spyder-py3')
+runfile('C:/Users/sidhh/.spyder-py3/app.py', wdir='C:/Users/sidhh/.spyder-py3')
+
+## ---(Fri Sep 30 18:53:11 2022)---
+runfile('C:/Users/sidhh/.spyder-py3/app.py', wdir='C:/Users/sidhh/.spyder-py3')
